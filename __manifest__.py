fb4062796d707497c0c8bd6db4a448385e463b38
miasnguyenx <miasnguyenx@gmail.com>
2023-09-12 14:05:21 +0700

    Done task 3

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "academy",
+    'name': "Akademy",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -28,7 +28,7 @@
         'views/views.xml',
         'views/templates.xml',
         'data/data.xml',
-        'demo/demo.xml',
+        # 'demo/demo.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/models.py
+++ models/models.py
@@ -1,28 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Teacher(models.Model):
     _name = 'academy.teacher'
     _description = 'encapsulate teachers information in academy'
 
-    name = fields.Char()
+    name = fields.Char(string="Teacher Name")
     biography = fields.Html()
 
     course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
     major_ids = fields.Many2many('academy.major', string="Majors")
+    
+    @api.model
+    def _create(self, data_list):
+        return super()._create(data_list)
 
 class Course(models.Model):
     _inherit = 'product.template'
     _description = 'encapsulate courses information'
-
-    name = fields.Char()
-    teacher_id = fields.Many2one('academy.teacher', string="Teacher")
-    major_ids = fields.Many2many('academy.major', string="Majors")
     
+    name = fields.Char(string="Course Name")
+    
+    teacher_id = fields.Many2one('academy.teacher', string="Teacher Name")
+    major_ids = fields.Many2many('academy.major', string="Majors")
+
+    @api.onchange('major_ids')
+    def _onchange_teacher_id(self):
+        teachers = self.env['academy.teacher'].search([('major_ids', 'in', self.major_ids.ids)])
+        logger.warning("============")
+        logger.warning(teachers.major_ids)
+        logger.warning(type(teachers.major_ids))
+        if self.major_ids.ids:
+            res = {
+                'domain':{
+                    'teacher_id':[(
+                        'id', 'in', teachers.ids,
+                    )]
+                }
+            }
+        else:
+            return
+        return res
+        
 class Major(models.Model):
     _name = 'academy.major'
     _description = "encapsulate majors information"
     
-    name = fields.Char()
+    name = fields.Char(string="Major Name")
